f93ac6cb5059b25c9b5a65bcb920897be8bb999a
Shreesh Nayak <shreeshnayak@gmail.com>
2020-09-26 15:34:16 -0700

    Installed spotify-web-api-js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 function App() {
 
