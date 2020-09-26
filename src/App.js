634c5aea0a87e8d45766a65f40c55b605ecd40b9
Shreesh Nayak <shreeshnayak@gmail.com>
2020-09-26 15:31:50 -0700

    Added spotify auth functionality and redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    //console.log("hash", hash);
+    window.location.hash = "";
+    const _token = hash.access_token;
+    
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
